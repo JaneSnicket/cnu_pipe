31715259b4d13935d0549c33a5d9d131c11df45b
Jessie <hyejeongje2219@gmail.com>
2026-05-23 21:02:26 +0900

    mlflow from local to ngrok

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 MODEL_MODE = "ml" # "rules" 
-MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
+MLFLOW_TRACKING_URI = "https://d700-125-183-16-57.ngrok-free.app"
+# MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
 MODEL_URI = "models:/spam-model@champion"
 TRAIN_FILE_NAME = "train.csv"
 TEST_FILE_NAME = "test.csv"
